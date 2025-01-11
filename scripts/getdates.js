b280fe12fba53890d842c72413c5b635771bb9bf
sferran2 <sferran@byupathway.edu>
2025-01-10 21:05:03 -0500

    update errors

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,4 @@ const currentYear = new Date().getFullYear();
 document.getElementById('current-year').textContent = currentYear;
 const currentDate = new Date();
 const formattedDate = currentDate.toLocaleString();
-document.querySelector('.last-modified').textContent = `Last Checked: ${formattedDate}`;
+document.querySelector('.last-modified').textContent = `Last Access: ${formattedDate}`;
