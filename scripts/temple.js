f184e9141400595acbfe454a2daeab9824fca9fa
sferran2 <sferran@byupathway.edu>
2025-01-16 09:18:39 -0500

    update hamburguer menu

--- scripts/temple.js
+++ scripts/temple.js
@@ -4,10 +4,11 @@ const currentDate = new Date();
 const formattedDate = currentDate.toLocaleString();
 document.querySelector('.last-modified').textContent = `Last Access: ${formattedDate}`;
 
+
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
-  navigation.classList.toggle('open'); // Toggle menu visibility
-  hamButton.textContent = hamButton.textContent === '☰' ? 'X' : '☰'; // Toggle button icon
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
 });
